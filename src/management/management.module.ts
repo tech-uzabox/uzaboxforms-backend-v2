7bbcc32dbc54d06a483b459f79bd8a132b929caf
Aurumdev952 <benjamugi20072@gmail.com>
2025-09-20 18:31:56 +0200

    fixed

--- src/management/management.module.ts
+++ src/management/management.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ManagementController } from './management.controller';
 import { ManagementService } from './management.service';
 import { FileService } from '../file/file.service';
+import { FileModule } from 'src/file/file.module';
 
 @Module({
+  imports: [FileModule],
   controllers: [ManagementController],
-  providers: [ManagementService, FileService]
+  providers: [ManagementService]
 })
 export class ManagementModule {}
