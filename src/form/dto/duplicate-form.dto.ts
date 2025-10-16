f7ed456398c0b18eae1da358db0d8c23eb8475ea
Aurumdev952 <benjamugi20072@gmail.com>
2025-10-16 22:06:03 +0200

    fix:

--- src/form/dto/duplicate-form.dto.ts
+++ src/form/dto/duplicate-form.dto.ts
@@ -3,8 +3,7 @@ import { createZodDto } from 'nestjs-zod';
 import { z } from 'zod';
 
 const DuplicateFormSchema = z.object({
-  formId: z.string().uuid(),
-  creatorId: z.string().uuid(),
+  formId: z.string().uuid()
 });
 
 export class DuplicateFormDto extends createZodDto(DuplicateFormSchema) {}
